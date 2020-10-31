e26d3aeebb290b0d4a1faedef9ffa5036e6dbff4
1w1111789AimonettiJoaquin <69583712+1w1111789AimonettiJoaquin@users.noreply.github.com>
2020-10-31 17:14:28 -0300

    ABM Producto

---
+++ AbmProducto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DistribuidoraCrelech
+{
+    public partial class AbmProducto : Form
+    {
+        public AbmProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AbmProducto_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblIdProd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace DistribuidoraCrelech
+{
+    partial class AbmProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIdProd = new System.Windows.Forms.TextBox();
+            this.lblIdProd = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblTipoProd = new System.Windows.Forms.Label();
+            this.cboTipoProd = new System.Windows.Forms.ComboBox();
+            this.cboRubro = new System.Windows.Forms.ComboBox();
+            this.lblRubro = new System.Windows.Forms.Label();
+            this.cboColor = new System.Windows.Forms.ComboBox();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStockMin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrecioVenta = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIdProd
+            // 
+            this.txtIdProd.Location = new System.Drawing.Point(390, 20);
+            this.txtIdProd.Name = "txtIdProd";
+            this.txtIdProd.Size = new System.Drawing.Size(150, 20);
+            this.txtIdProd.TabIndex = 0;
+            // 
+            // lblIdProd
+            // 
+            this.lblIdProd.AutoSize = true;
+            this.lblIdProd.Location = new System.Drawing.Point(387, 4);
+            this.lblIdProd.Name = "lblIdProd";
+            this.lblIdProd.Size = new System.Drawing.Size(62, 13);
+            this.lblIdProd.TabIndex = 1;
+            this.lblIdProd.Text = "Id Producto";
+            this.lblIdProd.Click += new System.EventHandler(this.lblIdProd_Click);
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(387, 43);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.TabIndex = 3;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(390, 59);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(150, 20);
+            this.txtDescripcion.TabIndex = 2;
+            // 
+            // lblTipoProd
+            // 
+            this.lblTipoProd.AutoSize = true;
+            this.lblTipoProd.Location = new System.Drawing.Point(387, 238);
+            this.lblTipoProd.Name = "lblTipoProd";
+            this.lblTipoProd.Size = new System.Drawing.Size(74, 13);
+            this.lblTipoProd.TabIndex = 5;
+            this.lblTipoProd.Text = "Tipo Producto";
+            // 
+            // cboTipoProd
+            // 
+            this.cboTipoProd.FormattingEnabled = true;
+            this.cboTipoProd.Location = new System.Drawing.Point(390, 254);
+            this.cboTipoProd.Name = "cboTipoProd";
+            this.cboTipoProd.Size = new System.Drawing.Size(150, 21);
+            this.cboTipoProd.TabIndex = 6;
+            // 
+            // cboRubro
+            // 
+            this.cboRubro.FormattingEnabled = true;
+            this.cboRubro.Location = new System.Drawing.Point(390, 294);
+            this.cboRubro.Name = "cboRubro";
+            this.cboRubro.Size = new System.Drawing.Size(150, 21);
+            this.cboRubro.TabIndex = 8;
+            // 
+            // lblRubro
+            // 
+            this.lblRubro.AutoSize = true;
+            this.lblRubro.Location = new System.Drawing.Point(387, 278);
+            this.lblRubro.Name = "lblRubro";
+            this.lblRubro.Size = new System.Drawing.Size(36, 13);
+            this.lblRubro.TabIndex = 7;
+            this.lblRubro.Text = "Rubro";
+            // 
+            // cboColor
+            // 
+            this.cboColor.FormattingEnabled = true;
+            this.cboColor.Location = new System.Drawing.Point(390, 334);
+            this.cboColor.Name = "cboColor";
+            this.cboColor.Size = new System.Drawing.Size(150, 21);
+            this.cboColor.TabIndex = 10;
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(387, 318);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(31, 13);
+            this.lblColor.TabIndex = 9;
+            this.lblColor.Text = "Color";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Stock";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(390, 176);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(150, 20);
+            this.txtStock.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Stock Minimo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtStockMin
+            // 
+            this.txtStockMin.Location = new System.Drawing.Point(390, 137);
+            this.txtStockMin.Name = "txtStockMin";
+            this.txtStockMin.Size = new System.Drawing.Size(150, 20);
+            this.txtStockMin.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(388, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Precio Venta";
+            // 
+            // txtPrecioVenta
+            // 
+            this.txtPrecioVenta.Location = new System.Drawing.Point(390, 98);
+            this.txtPrecioVenta.Name = "txtPrecioVenta";
+            this.txtPrecioVenta.Size = new System.Drawing.Size(150, 20);
+            this.txtPrecioVenta.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(389, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Precio por mayor";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(390, 215);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(181, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(189, 329);
+            this.listBox1.TabIndex = 19;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 35);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Nuevo";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 35);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Editar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 35);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Borrar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 170);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 35);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Grabar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(12, 211);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(163, 35);
+            this.btnCancelar.TabIndex = 24;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(12, 252);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(163, 35);
+            this.btnSalir.TabIndex = 25;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // AbmProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 384);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrecioVenta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtStockMin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.cboColor);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.cboRubro);
+            this.Controls.Add(this.lblRubro);
+            this.Controls.Add(this.cboTipoProd);
+            this.Controls.Add(this.lblTipoProd);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblIdProd);
+            this.Controls.Add(this.txtIdProd);
+            this.Name = "AbmProducto";
+            this.Text = "ABM Producto";
+            this.Load += new System.EventHandler(this.AbmProducto_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIdProd;
+        private System.Windows.Forms.Label lblIdProd;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblTipoProd;
+        private System.Windows.Forms.ComboBox cboTipoProd;
+        private System.Windows.Forms.ComboBox cboRubro;
+        private System.Windows.Forms.Label lblRubro;
+        private System.Windows.Forms.ComboBox cboColor;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStockMin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrecioVenta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
