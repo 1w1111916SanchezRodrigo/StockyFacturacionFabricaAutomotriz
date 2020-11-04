b9fff352655cc0c12514adf29e2dfd0f9ed5d370
1W1111991MartinezJoaquin <69582215+1W1111991MartinezJoaquin@users.noreply.github.com>
2020-11-04 09:02:38 -0300

    reportes mejorado

--- frmReportes.Designer.cs
+++ frmReportes.Designer.cs
@@ -66,7 +66,6 @@
             this.reportViewer1.ServerReport.BearerToken = null;
             this.reportViewer1.Size = new System.Drawing.Size(920, 327);
             this.reportViewer1.TabIndex = 0;
-            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
             // 
             // DataTable1TableAdapter
             // 
--- frmReportes.cs
+++ frmReportes.cs
@@ -13,6 +13,10 @@ namespace DistribuidoraCrelech
     public partial class frmReportes : Form
     {
         ConexionDatos Conexion = new ConexionDatos();
+        string strSQL = "select pr.id_producto 'Codigo',pr.descripcion 'Descripcion', tp.descripcion 'Tipo', ru.descripcion 'Rubro', ma.descripcion 'Material',"
+                           + "co.descripcion 'Color', pr.stock 'Stock', pr.stock_minimo 'StockMinimo', precio_mayo 'PrecioMayo', pr.precio_venta 'PrecioVenta'"
+                            + " from productos pr join tipos_productos tp on pr.id_tipo_producto = tp.id_tipo_producto join RUBROS ru on ru.id_rubro = pr.id_rubro left"
+                            + " join MATERIALES ma on ma.id_material = pr.id_producto left join COLORES co on co.id_color = pr.id_color";
         public frmReportes()
         {
             InitializeComponent();
@@ -26,10 +30,7 @@ namespace DistribuidoraCrelech
 
         private void btnImprimir_Click(object sender, EventArgs e)
         {
-            string strSQL = "select pr.id_producto 'Codigo',pr.descripcion 'Descripcion', tp.descripcion 'Tipo', ru.descripcion 'Rubro', ma.descripcion 'Material',"
-                           + "co.descripcion 'Color', pr.stock 'Stock', pr.stock_minimo 'StockMinimo', precio_mayo 'PrecioMayo', pr.precio_venta 'PrecioVenta'"
-                            + " from productos pr join tipos_productos tp on pr.id_tipo_producto = tp.id_tipo_producto join RUBROS ru on ru.id_rubro = pr.id_rubro left"
-                            + " join MATERIALES ma on ma.id_material = pr.id_producto left join COLORES co on co.id_color = pr.id_color";
+            
             //string strSQL = "select p.id_producto, p.descripcion, p.precio_venta, p.precio_mayo from PRODUCTOS p join tipos_productos tp on p.id_tipo_producto = tp.id_tipo_producto where tp.descripcion like '"+ cboTipoProducto.Text+"%'";
             //if (cboTipoProducto.SelectedIndex >= 0)
             //{
@@ -69,33 +70,22 @@ namespace DistribuidoraCrelech
 
             if (string.IsNullOrEmpty(txtPrecio.Text))
             {
-                string strSQL = $"select pr.id_producto 'Codigo',pr.descripcion 'Descripcion', tp.descripcion 'Tipo', ru.descripcion 'Rubro', ma.descripcion 'Material',"
-                + $"co.descripcion 'Color', pr.stock 'Stock', pr.stock_minimo 'StockMinimo', pr.precio_mayo 'PrecioMayo', pr.precio_venta 'PrecioVenta'"
-                + $" from productos pr join tipos_productos tp on pr.id_tipo_producto = tp.id_tipo_producto join RUBROS ru on ru.id_rubro = pr.id_rubro left"
-                + $" join MATERIALES ma on ma.id_material = pr.id_producto left join COLORES co on co.id_color = pr.id_color"
-                + $" where tp.descripcion like '{cboTipoProducto.Text}%'";
+                strSQL += $" where tp.descripcion like '{cboTipoProducto.Text}%'";
                 
                 DataTable1BindingSource.DataSource = Conexion.consulta(strSQL);
                 reportViewer1.RefreshReport();
             }
             else
             {
-                string strSQL = $"select pr.id_producto 'Codigo',pr.descripcion 'Descripcion', tp.descripcion 'Tipo', ru.descripcion 'Rubro', ma.descripcion 'Material',"
-                + $"co.descripcion 'Color', pr.stock 'Stock', pr.stock_minimo 'StockMinimo', pr.precio_mayo 'PrecioMayo', pr.precio_venta 'PrecioVenta'"
-                + $" from productos pr join tipos_productos tp on pr.id_tipo_producto = tp.id_tipo_producto join RUBROS ru on ru.id_rubro = pr.id_rubro left"
-                + $" join MATERIALES ma on ma.id_material = pr.id_producto left join COLORES co on co.id_color = pr.id_color"
-                + $" where tp.descripcion like '{cboTipoProducto.Text}%' and pr.precio_venta > {txtPrecio.Text}";
-
-                DataTable1BindingSource.DataSource = Conexion.consulta(strSQL);
+                string strSQL1 = strSQL + " where tp.descripcion like '"+cboTipoProducto.Text + "%' and pr.precio_venta > " + txtPrecio.Text;
+                
+                DataTable1BindingSource.DataSource = Conexion.consulta(strSQL1);
                 reportViewer1.RefreshReport();
             }
 
 
         }
 
-        private void reportViewer1_Load(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
